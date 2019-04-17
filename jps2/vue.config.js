2715492b4cffcb4fd369d5eb6c9a9abb02efad16
Satoru Nakamura <na.kamura.1263@gmail.com>
2019-04-18 00:02:17 +0900

    add vue

---
+++ jps2/vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  assetsDir: './',
+  publicPath: './',
+  css: {
+    loaderOptions: {
+      css: {
+        sourceMap: process.env.NODE_ENV !== "production" ? true : false
+      }
+    }
+  }
+};
